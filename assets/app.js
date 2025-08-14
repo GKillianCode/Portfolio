6eea88e620503fc21674debd663afcf10ac47423
Killian Godet <godetkillian@outlook.com>
2025-08-14 22:01:49 +0200

    Add fonctionnal darkmode

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,16 @@
 import "./styles/app.css";
+
+const body = document.querySelector("body");
+const darkModeButton = document.querySelector(".theme-controller");
+
+darkModeButton.addEventListener("click", () => {
+    const isButtonCheck = darkModeButton.checked;
+
+    if (isButtonCheck) {
+        body.dataset.theme = "dark";
+        localStorage.setItem("theme", "dark");
+    } else {
+        body.dataset.theme = "light";
+        localStorage.setItem("theme", "light");
+    }
+});
