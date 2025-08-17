8193e1297c08f9e1fad4c00c6d35f7a880c04539
Killian Godet <godetkillian@outlook.com>
2025-08-17 21:18:17 +0200

    Remove test.php

--- src/Controller/Admin/ProjectCrudController.php
+++ src/Controller/Admin/ProjectCrudController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Project;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
--- src/Controller/Admin/SkillTagCrudController.php
+++ src/Controller/Admin/SkillTagCrudController.php
@@ -3,9 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\SkillTag;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
