da8d3918b358f0aba1cf49791504513a2f665a92
Killian Godet <godetkillian@outlook.com>
2025-08-21 22:47:24 +0200

    Update webpack encore

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const Encore = require("@symfony/webpack-encore");
+
+Encore.setOutputPath("public/build/")
+    .setPublicPath("/build")
+    .addEntry("app", "./assets/app.js")
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enablePostCssLoader();
+
+module.exports = Encore.getWebpackConfig();
